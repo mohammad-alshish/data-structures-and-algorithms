e0536ac4641a727f8be19a40cae73ac2a12af093
mohammad-alshish <eng.moahammed.majed@gamil.com>
2022-06-26 16:06:44 +0300

    Space/time complextiy added to code file

--- stack_and_queue/stack_and_queue.py
+++ stack_and_queue/stack_and_queue.py
@@ -1,18 +1,28 @@
 class Node():
+    
+    # Space: O(1)
+    # Time: O(1)
     def __init__(self, value, next = None):
         self.value = value
         self.next = next
 
 
 class Stack():
+
+    # Space: O(1)
+    # Time: O(1)
     def __init__(self, node=None):
         self.top = node
 
+    # Space: O(1)
+    # Time: O(1)
     def push(self, value):
         node = Node(value)
         node.next = self.top
         self.top = node
 
+    # Space: O(1)
+    # Time: O(1)
     def pop (self):
         if self.is_empty():
             raise BaseException("The stack is empty")
@@ -21,20 +31,29 @@ class Stack():
            self.top = self.top.next
            return node.value
 
+    # Space: O(1)
+    # Time: O(1)
     def peek(self):
         if self.is_empty():
             raise BaseException("The stack is empty")
         return self.top.value
 
+    # Space: O(1)
+    # Time: O(1)
     def is_empty(self):
         return self.top == None
 
 
 class Queue():
+
+    # Space: O(1)
+    # Time: O(1)
     def __init__(self, front=None):
         self.front = front
         self.back = None
 
+    # Space: O(1)
+    # Time: O(1)
     def enqueue(self, value):
         node = Node(value)
         if self.is_empty():
@@ -44,6 +63,8 @@ class Queue():
             self.back.next = node
             self.back = node
 
+    # Space: O(1)
+    # Time: O(1)
     def dequeue(self):
         if self.is_empty():
             raise BaseException("The stack is empty")
@@ -51,10 +72,14 @@ class Queue():
         self.front = self.front.next
         return temp.value
 
+    # Space: O(1)
+    # Time: O(1)
     def peek(self):
         if self.is_empty():
             raise BaseException("The stack is empty")
         return self.front.value
 
+    # Space: O(1)
+    # Time: O(1)
     def is_empty(self):
         return self.front == None
